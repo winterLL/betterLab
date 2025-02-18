871a713b33a06ebafab0360e1e56f8319871f760
Kristi Pikioni <79057774+winterLL@users.noreply.github.com>
2025-02-18 23:42:45 +0200

    Add files via upload

---
+++ lab/pages/profile.php
@@ -0,0 +1,17 @@
+<?php
+    // include '../functions/generic.php';
+    include ('../functions/db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="categories"></div>
+    <div class="list"></div>
+    <div class="notes"></div>
+</body>
+</html>
